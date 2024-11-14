34280e3a6d07972c52873cf971fd374eda10c842
davy_chen <davy_chen@epam.com>
2024-11-14 20:21:02 +0800

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,1 +1,45 @@
 
+{
+  layout: {
+    // items are chilren for layout component
+    items: [
+      {
+        component: 'search',
+        items: [
+          {
+            component: 'input',
+            id: '',
+            formItemProps: {},
+            componentProps: {}
+          }
+        ],
+        props: {
+            // search is a form kind component, the props will pass to a Form
+        },
+        "feature-toggle": {
+          resetButtonHidden: Boolean,
+          resetButtonText: 'Reset',
+          searchButtonText: 'Search'
+        }
+      }
+    ],
+    component: 'flex'.
+    props: {
+      // props will as props pass to component 
+    }
+  },
+  layout: {
+    component: 'flex'.
+    props: {
+      // props will as props pass to component 
+    },
+    item: {
+      component: 'table',
+      props: {},
+      config: {
+        rtkQuery: function,
+      }
+    }
+  }
+}
+ 
